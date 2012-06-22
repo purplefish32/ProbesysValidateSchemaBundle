ba5d70fe2e5089a70e2d980b53afb85498bce948
Donovan Tengblad <donovan.tengblad@probesys.com>
2012-06-22 14:38:36 +0200

    Edited description

--- Command/ProbesysValidateSchemaCommand.php
+++ Command/ProbesysValidateSchemaCommand.php
@@ -1,25 +1,25 @@
 <?php
 namespace Probesys\ValidateSchemaBundle\Command;
- 
+
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Bundle\DoctrineBundle\Command\Proxy\DoctrineCommandHelper;
 use Doctrine\ORM\Tools\Console\Command\ValidateSchemaCommand;
- 
-class MyValidateSchemaCommand extends ValidateSchemaCommand
+
+class ProbesysValidateSchemaCommand extends ValidateSchemaCommand
 {
 	protected function configure()
 	{
 		parent::configure();
 		$this->setName('probesys:doctrine:orm:validate-schema');
+		$this->setDescription('Validate that the mapping files are correct and in sync with the database.');
 		$this->addOption('em', null, InputOption::VALUE_OPTIONAL, 'The entity manager to use for this command');
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
 		DoctrineCommandHelper::setApplicationEntityManager($this->getApplication(), $input->getOption('em'));
- 
-                return parent::execute($input, $output);
+		return parent::execute($input, $output);
 	}
 }
